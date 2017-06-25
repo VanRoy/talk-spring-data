5e73a957778dd5028bf7e024229f4412955ea9a4
Julien Roy <julien.roy@ekino.com>
2017-06-25 15:56:31 +0200

    Upgrade Spring Boot version

--- reactive/src/main/java/com/github/vanroy/springdata/reactive/config/Config.java
+++ reactive/src/main/java/com/github/vanroy/springdata/reactive/config/Config.java
@@ -1,7 +1,5 @@
 package com.github.vanroy.springdata.reactive.config;
 
-import javax.annotation.PostConstruct;
-
 import com.github.vanroy.springdata.reactive.model.Person;
 import com.github.vanroy.springdata.reactive.service.NotificationService;
 import org.springframework.context.annotation.Bean;
@@ -9,10 +7,12 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.core.CollectionOptions;
 import org.springframework.data.mongodb.core.ReactiveMongoTemplate;
 import org.springframework.web.servlet.config.annotation.AsyncSupportConfigurer;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import javax.annotation.PostConstruct;
 
 @Configuration
-public class Config extends WebMvcConfigurerAdapter {
+public class Config implements WebMvcConfigurer {
 
     private final ReactiveMongoTemplate template;
 
@@ -29,7 +29,7 @@ public class Config extends WebMvcConfigurerAdapter {
     void initMongoCollection() {
 
         template.dropCollection("person").block();
-        template.createCollection("person", new CollectionOptions(1024 * 1024, 100, true)).block();
+        template.createCollection("person", CollectionOptions.empty().size(1024 * 1024).maxDocuments(100).capped()).block();
 
         template.save(new Person("Homer", "Simpson", 42)).subscribe();
 
--- reactive/src/main/java/com/github/vanroy/springdata/reactive/repository/PersonRepository.java
+++ reactive/src/main/java/com/github/vanroy/springdata/reactive/repository/PersonRepository.java
@@ -1,12 +1,11 @@
 package com.github.vanroy.springdata.reactive.repository;
 
-import java.util.UUID;
-
 import com.github.vanroy.springdata.reactive.model.Person;
-import org.reactivestreams.Publisher;
-import org.springframework.data.mongodb.repository.InfiniteStream;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import org.springframework.data.mongodb.repository.Tailable;
 import reactor.core.publisher.Flux;
+
+import java.util.UUID;
 
 /**
  * Person repository.
@@ -14,7 +13,7 @@ import reactor.core.publisher.Flux;
  */
 public interface PersonRepository extends ReactiveCrudRepository<Person, UUID> {
 
-    @InfiniteStream
+    @Tailable
     Flux<Person> findBy();
 
     Flux<Person> findByAgeGreaterThan(int age);
